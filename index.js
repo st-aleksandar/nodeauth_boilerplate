57efecf294d57ff51078791f15e6df6565cf68b6
Aleksandar Stojanovic <aca_fpi@hotmail.com>
2019-01-16 11:19:20 +0100

    CORS added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:27017/auth', {useNewUrlParser: true});
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/auth', {useNewUrlParser: true});
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
